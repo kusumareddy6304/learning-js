c4bf54c8921d1d6ce94966e411b42f7b700f4c0a
kusumareddy6304 <kusumareddy6304@gmail.com>
2026-04-11 09:27:03 +0530

    added file

---
+++ day2/index.js
@@ -0,0 +1,34 @@
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+// let c = 32;
+// console.log(c);
+
+// const b = 54;
+// console.log(b);
+
+// let num1 = 10;
+// let num2 = "10";
+// if(num1==num2){
+//     console.log("-1");
+// }else{
+//     console.log("1");
+// }
+
+// let x = (2*3)
+// console.log(x);
+// let y = (2**3)
+// console.log(y);
+
+
+
+//arrays
+
+// let userData = [10,"hello",false,3.42,'bye'];
+
+// for(let index = 0;index < userData.length; index++){
+//     console.log( userData[index]);
+    
+//     // console.log(typeof userData[index]);  
+// }
