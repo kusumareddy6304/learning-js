1c38936859d4aa464ad3ccaf47ef5c441ab6d69c
kusumareddy6304 <kusumareddy6304@gmail.com>
2026-04-13 09:12:18 +0530

    added day3

---
+++ day3/index.js
@@ -0,0 +1,43 @@
+// const a = [10,20]
+// a[0] = 30;
+// console.log(a);
+
+
+// const obj = {
+//     id:1,
+//     name:"guest",
+//     myData: [10,"hello","bye bye"]
+    
+// }
+// console.log(obj);
+// console.log(obj.name);
+// console.log(obj.id)
+//console.log(obj.myData);
+
+//adding the elements in object
+// obj.loc = "hyd"
+
+// console.log(obj);
+
+const users = [
+    {
+    id : 1,
+    name : "user1",
+    loc : "hyd"
+    },
+    {
+    id : 2,
+    name : "user2",
+    loc : "hyd"
+    },
+]
+console.log(users[0].name);
+console.log(users[0]);
+console.log(users[1].id);
+
+// for of - arrays
+// for in - objects
+
+
+
+
